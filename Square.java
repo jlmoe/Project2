a13e5a1e848d283b4f764e6fb5d49f625993f15e
Jeffrey Moe <31293711+jlmoe@users.noreply.github.com>
2017-10-05 11:04:04 -0400

    Finished operations
    Completed all method required and added a toString override

--- Square.java
+++ Square.java
@@ -1,7 +1,6 @@
-import java.awt.*;
 public class Square extends GraphicObject{
 	private double sideLength;
-	
+
 	public Square(double newX, double newY, int red, int green, int blue, double newSideLength){
 		super(newX, newY, red, green, blue);
 		sideLength = newSideLength;
@@ -9,23 +8,28 @@ public class Square extends GraphicObject{
 
 	@Override
 	public void draw() {
-		// TODO Auto-generated method stub
-		
+		StdDraw.setPenColor(color);		
+		StdDraw.square(x, y, sideLength);
 	}
 
 	@Override
 	public double calculateArea() {
-		// TODO Auto-generated method stub
-		return 0;
+		double area = Math.pow((2*sideLength),2);
+		return area;
 	}
 
 	@Override
 	public double calculatePerimeter() {
-		// TODO Auto-generated method stub
-		return 0;
+		double perimeter = sideLength*4;
+		return perimeter;
 	}
-	
+
 	public double getSideLength(){
 		return sideLength;
+	}
+	
+	public String toString(){
+		String result = super.toString() + " " + sideLength + " ";
+		return result;
 	}
 }
