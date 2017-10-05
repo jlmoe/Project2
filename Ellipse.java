b3563af9b664c8991aa9f612ca334b02071d7c05
Jeffrey Moe <31293711+jlmoe@users.noreply.github.com>
2017-10-05 11:01:12 -0400

    Finished operation
    finished operation for class Ellipse, added a toString override

--- Ellipse.java
+++ Ellipse.java
@@ -1,7 +1,7 @@
-import java.awt.*;
 public class Ellipse extends GraphicObject{
 	private double semiMajorAxis;
 	private double semiMinorAxis;
+	
 	
 	public Ellipse(double newX, double newY, int red, int green, int blue, double newSemiMajor, double newSemiMinor){
 		super(newX, newY, red, green, blue);
@@ -11,20 +11,26 @@ public class Ellipse extends GraphicObject{
 
 	@Override
 	public void draw() {
-		// TODO Auto-generated method stub
-		
+		StdDraw.setPenColor(color);
+		StdDraw.ellipse(x, y, semiMajorAxis, semiMinorAxis);
 	}
 
 	@Override
 	public double calculateArea() {
-		// TODO Auto-generated method stub
-		return 0;
+		double area = semiMajorAxis*semiMinorAxis*Math.PI;
+		return area;
 	}
 
 	@Override
 	public double calculatePerimeter() {
-		// TODO Auto-generated method stub
-		return 0;
+		//the area is calculated by an infinite series as described here:https://www.mathsisfun.com/geometry/ellipse-perimeter.html
+		double perimeter = 0;
+		double h = Math.pow((semiMajorAxis - semiMinorAxis), 2) / Math.pow((semiMajorAxis + semiMinorAxis), 2);
+		for(int i = 0; i < 10; i++){	//bring the series out to 10 terms
+			perimeter += Math.PI * (semiMajorAxis + semiMinorAxis) * ((double)1 / (double)Math.pow(4, i)) * (double)Math.pow(h, i);
+		}
+		
+		return perimeter;
 	}
 	
 	public double getSemiMajor(){
@@ -34,4 +40,10 @@ public class Ellipse extends GraphicObject{
 	public double getSemiMinor(){
 		return semiMinorAxis;
 	}
+	
+	public String toString(){
+		String result = super.toString() + " " + semiMajorAxis + " " + semiMinorAxis + " ";
+		return result;
+	}
+	
 }
